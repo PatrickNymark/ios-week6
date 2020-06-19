bace0a1368d0a2aa21066e0f6063b3c857cd36de
Patrick Nymark <patricknymark24@gmail.com>
2020-06-19 21:40:08 +0200

    Initial Commit

---
+++ Company-Business-Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Company-Business-Card
+//
+//  Created by Patrick Nymark on 19/06/2020.
+//  Copyright © 2020 Patrick Nymark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
